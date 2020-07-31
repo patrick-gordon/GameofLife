10a51bab9ddd7e18596c2eab6cf08dbe75c6e721
patrick-gordon <patrickgordon42@yahoo.com>
2020-07-31 01:10:47 -0500

    game is running and generating random config's based on game rules

--- src/index.js
+++ src/index.js
@@ -49,6 +49,27 @@ class Grid extends React.Component {
 }
 
 
+class Buttons extends React.Component {
+  render(){
+    return(
+      <div>
+        <button onClick={this.props.playButton}>Play</button>
+        <button  onClick={this.props.pauseButton}>Pause</button>
+        <button  onClick={this.props.fast}>Fast</button>
+        <button  onClick={this.props.slow}>Slow</button>
+        <button  onClick={this.props.clear}>Clear</button>
+        <button  onClick={this.props.seed}>Seed</button>
+        <button  onClick={this.props.gridSize}>Grid Size</button>
+        
+      </div>
+    )
+  }
+}
+
+
+
+
+
 class Main extends React.Component {
   constructor(){
     super();
@@ -85,14 +106,61 @@ class Main extends React.Component {
     })
   }
 
+  playButton = () => {
+    clearInterval(this.intervalID)
+    this.intervalID = setInterval(this.play, this.speed);
+  }
+
+  pauseButton = () => {
+    clearInterval(this.intervalID)
+  }
+
+  play = () => {
+    let g = this.state.gridFull;
+    let g2 = arrayClone(this.state.gridFull);
+
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        let count = 0;
+        // check if neighbor is occupied
+        if (i > 0) if (g[i - 1][j]) count++;
+        if (i > 0 && j > 0) if(g[i - 1][j - 1]) count++;
+        if (i > 0 && j < this.cols - 1) if(g[i - 1][j + 1]) count++;
+        if (j < this.cols - 1) if(g[i][j + 1]) count++;
+        if (j > 0 ) if(g[i][j - 1]) count++;
+        if (i < this.rows - 1) if(g[i + 1][j]) count++;
+        if (i < this.rows - 1 && j > 0) if(g[i + 1][j - 1]) count++;
+        if (i < this.rows - 1 && this.cols - 1) if(g[i + 1][j + 1]) count++;
+        // cell lives or dies
+        if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false;
+        if (!g[i][j] && count === 3) g2[i][j] = true;
+      }
+    }
+    this.setState({
+      gridFull: g2,
+      generations: this.state.generations + 1
+    });
+  }
+
   componentDidMount(){
     this.seed();
+    this.playButton();
   }
 
   render(){
     return(
       <div>
         <h1>Game of Life</h1>
+        <Buttons
+        playButton={this.playButton}
+        pauseButton={this.pauseButton}
+        slow={this.slow}
+        fast={this.fast}
+        clear={this.clear}
+        seed={this.seed}
+        gridSize={this.gridSize}
+        />
+
         <Grid 
         gridFull={this.state.gridFull}
         rows={this.rows}
